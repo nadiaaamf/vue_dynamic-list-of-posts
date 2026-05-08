6a114bc05d23d4b05f2b915a46a669bb47bba6dc
Nadia Marcelino <nadia.mfaria@hotmail.com>
2026-05-08 16:03:26 -0300

    add logout buttom

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,31 @@
+<script setup>
+const emit = defineEmits(['logout'])
+
+function handleLogout() {
+  emit('logout')
+}
+</script>
+
 <template>
-<h1>Vue List Of Posts</h1>
+  <nav
+    class="navbar is-link px-4"
+    role="navigation"
+  >
+    <div class="navbar-brand">
+      <h1 class="navbar-item has-text-weight-bold is-size-4">
+        Dynamic Posts
+      </h1>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <button
+          class="button is-light"
+          @click="handleLogout"
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  </nav>
 </template>
